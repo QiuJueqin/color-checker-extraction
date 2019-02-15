dbee6e55c8d01e64233dd67b6a05a9dc80390e6f
QiuJueqin <jqx1991@gmail.com>
2019-02-15 20:51:28 +0800

    repo init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Color Checker Extraction Toolbox
+
+Color Checker Extraction Toolbox allows you to pick up color responses from images containing various types of color checkers, which is useful for camera color correction or other camera calibration applications. Currently only manual mode is supported. The auto mode will be included in the future using [Object Detection Framework](https://github.com/pedrodiamel/colorchecker-detection).
+
+MATLAB version R2018b or higher with Image Processing Toolbox is **recommended** in order to implement [images.roi](https://www.mathworks.com/help/images/roi-based-processing.html) object. But for other versions the normal [rectangle](https://www.mathworks.com/help/matlab/ref/rectangle.html) function works well too.
+
+Following screen records will give you a brief impression.
+
+# Demo
+
+* Interactively select color checker region in the image:
+
+<img src="screenshots/demo1.gif" width="1024">
+
+<br>
+
+* Programmatically specify Regions-of-Interest for color patches without need to manually select checkers again and again:
+
+<img src="screenshots/demo2.gif" width="1024">
+
+# License
+
+Copyright 2019 Qiu Jueqin
+
+Licensed under [MIT](http://opensource.org/licenses/MIT).
---
+++ demo.m
@@ -0,0 +1,26 @@
+%% demo1
+% Drag a rectangle in the image to exactly cover 4 corner markers in the
+% DSG ColorChecker. If the ROI for some patches are not locating at the
+% patch centers, you can manually adjust them and click 'Continue' to
+% finish.
+clear all; close all; clc;
+cc_img = imread('dsg.png');
+colors = checker2colors(cc_img, [10, 14], 'allowadjust', true, 'roisize', 50);
+checker = colors2checker(colors, 'array', [10, 14]);
+
+
+%% demo 2
+% Click 4 corners in the Passport ColorChecker successively to draw a
+% quadrangle exactly convering the color checker. Note that the first point
+% you click will be treated as the upper-left vertex, so in order to
+% retrieve colors in the correct sequence, you should click the
+% bottom-right marker first (the marker next to the dark skin patch).
+clear all; close all; clc;
+cc_img = imread('passport.jpg');
+% coord can be used for another extraction, without need to draw a
+% quadrangle again
+[~, coord] = checker2colors(cc_img, [4, 6], 'mode', 'click', 'show', false);
+clearvars -except coord
+cc_img = imread('passport.jpg');
+colors = checker2colors(cc_img, [4, 6], 'roi', coord);
+checker = colors2checker(colors); % not parsing 'array' parameter works well too
